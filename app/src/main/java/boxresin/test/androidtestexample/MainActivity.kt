4dd4c313cb429977ea23caa23136bab72967374c
Minsuk Eom <boxresin@gmail.com>
2019-08-11 03:55:50 +0900

    테스트를 통과하도록 메인 액티비티 수정

--- app/src/main/java/boxresin/test/androidtestexample/MainActivity.kt
+++ app/src/main/java/boxresin/test/androidtestexample/MainActivity.kt
@@ -1,12 +1,27 @@
 package boxresin.test.androidtestexample
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 /** 메인 액티비티 */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable) {
+                btn_login.isEnabled = edit_id.text.isNotEmpty() && edit_password.text.isNotEmpty()
+            }
+        }
+        this.edit_id.addTextChangedListener(textWatcher)
+        this.edit_password.addTextChangedListener(textWatcher)
     }
 }
